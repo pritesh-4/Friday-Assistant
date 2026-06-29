c92c7e2fd6e5ebbbaa5d17e4e6a043d77adcb3de
Pritesh jena <priteshjena16@gmail.com>
2026-06-29 13:30:23 +0530

    folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+import {reactDOM} from 'react-dom/c;ient'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+reactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter><App /></BrowserRouter>,
 )
