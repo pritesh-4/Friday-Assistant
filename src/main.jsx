b61e25e84ae78c7445f39f46f237d25eab1299bd
Pritesh jena <priteshjena16@gmail.com>
2026-06-30 21:03:18 +0530

    fix: existing bug-1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import {
+  ChatProvider,
+  SidebarProvider,
+  SettingsProvider,
+  MemoryProvider
+} from "./context";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter>
+    <SettingsProvider>
+      <SidebarProvider>
+        <MemoryProvider>
+          <ChatProvider>
+            <App />
+          </ChatProvider>
+        </MemoryProvider>
+      </SidebarProvider>
+    </SettingsProvider>
+  </BrowserRouter>,
 )
